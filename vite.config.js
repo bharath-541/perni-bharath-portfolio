2132d061fba3f517b9fd37d3b0dbe599195f611d
bharath-ayu <bharath@ayuapp.com>
2026-08-19 10:45:20 +0530

    Support Vercel root domain deployments

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/perni-bharath-portfolio/',
+  base: process.env.VERCEL ? '/' : '/perni-bharath-portfolio/',
   plugins: [react()],
 })
